a8c08a2bbf720eb4b194ea104bf6f1f78641eefb
nandhu-44 <nandhu90040@gmail.com>
2025-03-22 17:13:09 +0530

    Logo

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -15,6 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/diagnose-me-logo.svg" />
+      </head>
       <body
         className={`${inter.variable} antialiased bg-background`}
       >
