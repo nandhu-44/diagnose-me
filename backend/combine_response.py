c849bb6c19b13863382bbd16271fedab773d54d8
nazalnihad <nazalnihad17@gmail.com>
2025-03-23 09:27:09 +0530

    graph fix

--- backend/combine_response.py
+++ backend/combine_response.py
@@ -4,14 +4,16 @@ import combine_llm
 
 def combine(query):
     rag_response = query_db.query(query)
-    graph_response = gq.graph_query(query)
+    graph_response = gq.graph_query("")
     return rag_response, graph_response
 
-sample_text = "hello fever me bad shiver high temp what to do"
-combined_query_response = combine(sample_text)
+# sample_text = "hello fever me bad shiver high temp what to do"
+text="I've been dealing with diabetes, hypertension, hyperlipidemia, and obesity. Over the past couple of months, I’ve had chest burning with exertion, but it used to go away with rest. Lately, even walking brings it on, and now I’m also getting nausea, sweating, and shortness of breath. In the last two days, I’ve had symptoms even while resting. I recently had an exercise test that showed significant EKG changes, and today my cardiac catheterization revealed left main artery disease. I was given nitroglycerin, IV Lopressor, and Ativan, which helped, and an intra-aortic balloon pump was placed. Right now, I don’t have pain, but I’m worried about what all this means for my heart health. What should I do next?"
+
+combined_query_response = combine(text)
 
 # summarized_rag_response = combine_llm.summarize_response(combined_query_response[0])
-combined_response = combine_llm.get_response(sample_text, combined_query_response[0], combined_query_response[1])
+combined_response = combine_llm.get_response(text, combined_query_response[0], combined_query_response[1])
 
 print(combined_response)
 # print(summarized_rag_response)
--- backend/graphRAG/graph_query.py
+++ backend/graphRAG/graph_query.py
@@ -4,7 +4,7 @@ import os
 from langchain_community.graphs import Neo4jGraph
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from llm_response import get_response
+from .llm_response import get_response
 
 def graph_query(text_input):
     load_dotenv()
@@ -43,7 +43,7 @@ RETURN p.name AS Patient, mc.name AS Condition"""
     return result
 
 # Example usage
-sample_text = "What medications are used to treat pancreatic cancer and hypertension, and are there any overlapping treatments?"
-text="woman with diabetes, hypertension, hyperlipidemia and obesity, with a one to two months of chest burning with exertion. for the past six months, she has been participating in a new vigorous exercise program to lose weight. her symptoms do gradually resolve with rest, but they have started to occur now with walking. she does acknowledge that there is associated nausea, diaphoresis and shortness of breath. now recently she started to get symptoms for the past two days while at rest. she was referred for an outpatient exercise tolerance test, where she had chest pain and significant ekg changes. she was referred to for cardiac catheterization today, which revealed significant left main artery disease. just prior to her transfer to , she did complain of chest pain and back pain at about 7/10 intensity and a nitroglycerin drip was started and she received 5 mg of iv lopressor and 2 mg of iv ativan, which did resolve her pain. while she was in the cath lab, she had an intraaortic balloon pump placed, and now she is not actively complaining of any pain. review of systems: she denies any orthopnea, lower extremity edema, but she does acknowledge that she does have dysmenorrhea. prior medical history: she has diabetes mellitus, type 2, diagnosed in . she has been able to control it with diet since ; hypertension and obesity; however, she has lost 40 pounds in the last six months. she has an allergy to shellfish. medications on transfers to : atenolol 50 mg a day, lisinopril 10 mg a day, lipitor 20 mg a day, aspirin 325 mg once a day, progesterone, nitroglycerin drip, lopressor, ativan"
-result = graph_query(text)
-print(result["result"])
+# sample_text = "What medications are used to treat pancreatic cancer and hypertension, and are there any overlapping treatments?"
+# text="woman with diabetes, hypertension, hyperlipidemia and obesity, with a one to two months of chest burning with exertion. for the past six months, she has been participating in a new vigorous exercise program to lose weight. her symptoms do gradually resolve with rest, but they have started to occur now with walking. she does acknowledge that there is associated nausea, diaphoresis and shortness of breath. now recently she started to get symptoms for the past two days while at rest. she was referred for an outpatient exercise tolerance test, where she had chest pain and significant ekg changes. she was referred to for cardiac catheterization today, which revealed significant left main artery disease. just prior to her transfer to , she did complain of chest pain and back pain at about 7/10 intensity and a nitroglycerin drip was started and she received 5 mg of iv lopressor and 2 mg of iv ativan, which did resolve her pain. while she was in the cath lab, she had an intraaortic balloon pump placed, and now she is not actively complaining of any pain. review of systems: she denies any orthopnea, lower extremity edema, but she does acknowledge that she does have dysmenorrhea. prior medical history: she has diabetes mellitus, type 2, diagnosed in . she has been able to control it with diet since ; hypertension and obesity; however, she has lost 40 pounds in the last six months. she has an allergy to shellfish. medications on transfers to : atenolol 50 mg a day, lisinopril 10 mg a day, lipitor 20 mg a day, aspirin 325 mg once a day, progesterone, nitroglycerin drip, lopressor, ativan"
+# result = graph_query(text)
+# print(result["result"])
